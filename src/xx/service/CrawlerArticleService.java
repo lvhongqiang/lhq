5336906b1f0381ee988959da5f06eba69ae171c1
lhq <lvhongqiang09@163.com>
2016-12-04 21:12:34 +0800

    文章倒叙；修改定时器为每小时15分执行

--- src/xx/service/CrawlerArticleService.java
+++ src/xx/service/CrawlerArticleService.java
@@ -16,20 +16,20 @@ public class CrawlerArticleService {
     @Autowired private BaseDao baseDao;
 
     public Page list(Integer pageNo,Integer pageSize){
-        return baseDao.findPage("from CrawlerArticle","select count(*) from CrawlerArticle",pageNo, pageSize);
+        return baseDao.findPage("from CrawlerArticle order by id desc","select count(*) from CrawlerArticle",pageNo, pageSize);
     }
 
     public CrawlerArticle get(Integer id) {
         return (CrawlerArticle)baseDao.get(CrawlerArticle.class,id);
     }
     public CrawlerArticle pre(Integer id) {
-        List<CrawlerArticle> list = baseDao.findList("from CrawlerArticle where id<? order by id desc", id, 0, 1);
+        List<CrawlerArticle> list = baseDao.findList("from CrawlerArticle where id>? order by id asc", id, 0, 1);
         if(list!=null&&list.size()>0)
             return list.get(0);
         return null;
     }
     public CrawlerArticle next(Integer id) {
-        List<CrawlerArticle> list = baseDao.findList("from CrawlerArticle where id>? order by id asc", id, 0, 1);
+        List<CrawlerArticle> list = baseDao.findList("from CrawlerArticle where id<? order by id desc", id, 0, 1);
         if(list!=null&&list.size()>0)
             return list.get(0);
         return null;
